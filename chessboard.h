b8e9da2c0eebd4a22717fe2a5285c2b093cd4123
Liam Andrus <andrliam@isu.edu>
2023-03-14 11:50:05 -0600

    fixed capture bug

--- chessboard.h
+++ chessboard.h
@@ -7,16 +7,17 @@ const int ROW_COUNT = 6;
 const int COL_COUNT = 6;
 
 class BoardCell;
+class ChessGame;
 
 class ChessBoard
 {
     public:
     BoardCell* cells[ROW_COUNT][COL_COUNT];
+    ChessGame* parentGame;
 
     ChessBoard();
     BoardCell* getCell(int rowIndex, int colIndex);
     void print();
-
 };
 
 #endif // CHESSBOARD_H
--- player.cpp
+++ player.cpp
@@ -1,9 +1,15 @@
 #include "player.h"
+#include <iostream>
+
+using namespace std;
 
 Player::Player(Color color){
     this->color = color;
 }
 
 void Player::addCapturedPiece(ChessPiece* piece){
-//    capturedPieces.push_back(piece);
+    cout << capturedPieces.size() << endl;
+    capturedPieces.push_back(piece);
+    cout << capturedPieces.size() << endl;
+    cout << "piece added to player's captured pieces" << endl;
 }
